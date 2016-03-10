805fdb8e331d9db9a25aed71f5c228b10cb8b44a
Andrii Shynkar <a-shinkar@hotmail.com>
2016-03-11 00:25:58 +0200

    Initial commit

---
+++ Array_2.php
@@ -0,0 +1,42 @@
+<?php
+$array = [
+    ['Name' => 'Artem', 'Surname' => 'Melnyk','Age'  => '38'],
+    ['Name' => 'Dima', 'Surname' => 'Shevchenko', 'Age'  => '25'],
+    ['Name' => 'Sergey', 'Surname' => 'Boyko', 'Age'  => '28'],
+    ['Name' => 'Andrey', 'Surname' => 'Kovalenko', 'Age'  => '24'],
+    ['Name' => 'Vlad', 'Surname' => 'Bondarenko', 'Age'  => '35'],
+    ['Name' => 'Ivan', 'Surname' => 'Tkachenko', 'Age'  => '60'],
+    ['Name' => 'Anton', 'Surname' => 'Kovalchuk', 'Age'  => '42'],
+    ['Name' => 'Maxim', 'Surname' => 'Kravchenko', 'Age'  => '57'],
+    ['Name' => 'Oleg', 'Surname' => 'Oliynyk', 'Age'  => '55'],
+    ['Name' => 'Roman', 'Surname' => 'Shevchuk', 'Age'  => '18'],
+    ['Name' => 'Nikita', 'Surname' => 'Polishchuk', 'Age'  => '20'],
+    ['Name' => 'Alex', 'Surname' => 'Lysenko', 'Age'  => '21'],
+    ['Name' => 'Artem', 'Surname' => 'Lysenko', 'Age'  => '54'],
+    ['Name' => 'Dima', 'Surname' => 'Polishchuk', 'Age'  => '59'],
+    ['Name' => 'Sergey', 'Surname' => 'Shevchuk', 'Age'  => '58'],
+    ['Name' => 'Andrey', 'Surname' => 'Oliynyk', 'Age'  => '41'],
+    ['Name' => 'Anton', 'Surname' => 'Kravchenko', 'Age'  => '45'],
+    ['Name' => 'Ivan', 'Surname' => 'Kovalchuk', 'Age'  => '47'],
+    ['Name' => 'Anton', 'Surname' => 'Bondarenko', 'Age'  => '29'],
+    ['Name' => 'Roman', 'Surname' => 'Kovalenko', 'Age'  => '30'],
+    ['Name' => 'Oleg', 'Surname' => 'Boyko', 'Age'  => '33'],
+    ['Name' => 'Roman', 'Surname' => 'Shevchenko', 'Age'  => '34'],
+    ['Name' => 'Nikita', 'Surname' => 'Melnyk', 'Age'  => '37'],
+    ['Name' => 'Alex', 'Surname' => 'Tkachenko', 'Age'  => '39'],
+    ['Name' => 'Roman', 'Surname' => 'Tkachenko', 'Age'  => '53'],
+    ['Name' => 'Oleg', 'Surname' => 'Melnyk', 'Age'  => '54'],
+    ['Name' => 'Roman', 'Surname' => 'Oliynyk', 'Age'  => '26'],
+    ['Name' => 'Anton', 'Surname' => 'Kovalchuk', 'Age'  => '23'],
+    ['Name' => 'Anton', 'Surname' => 'Lysenko', 'Age'  => '29'],
+    ['Name' => 'Sergey', 'Surname' => 'Polishchuk', 'Age'  => '22'],
+];
+
+echo "<pre>";
+print_r ($array);
+echo "</pre>";
+
+
+?>
+
+
